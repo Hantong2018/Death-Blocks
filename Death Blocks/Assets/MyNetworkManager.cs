2079df91ba1d63f02d14a5bed5ed5a884821776f
Popfer <32405025+Popfer@users.noreply.github.com>
2018-04-12 17:02:26 -1000

    IDK what to put

---
+++ Death Blocks/Assets/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : MonoBehaviour {
+    private NetworkManager networkManager;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+	}
+
+    public void MyStartHost () {
+        Debug.Log("Starting host at" + Time.timeSinceLevelLoad);
+        networkManager.StartHost();
+    }
+	
+	// Update is called once per frame
+	override void Update () {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+	}
+}
